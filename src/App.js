ba3086a403b83eefe423aab6a3058a8da8e9de27
Bernardo Euler <rasta.dev07@gmail.com>
2022-07-15 22:47:18 -0300

    Add conditional rendering based on authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import {
-  Center,
-  NativeBaseProvider,
-  Spinner,
-} from "native-base"
+import { useState, useEffect } from "react"
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
@@ -12,37 +8,50 @@ import { useFonts } from "expo-font"
 import interFonts from "./assets/fonts/inter"
 import josefinSansFonts from "./assets/fonts/josefin-sans"
 
-import theme from "./config/theme"
-
+import Loading from "./pages/Loading"
 import Welcome from "./pages/Welcome"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import Home from "./pages/Home"
+
+import { auth } from "./config/firebase"
+import { onAuthStateChanged } from "firebase/auth"
 
 function App() {
   const Stack = createNativeStackNavigator()
 
+  const [user, setUser] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
   const [fontsLoaded] = useFonts({
     ...interFonts,
     ...josefinSansFonts
   })
 
-  if (!fontsLoaded) {
-    return (
-      <NativeBaseProvider theme={theme}>
-        <Center flex="1">
-          <Spinner color="neutral.50" size={64}/>
-        </Center>
-      </NativeBaseProvider>
-    )
-  }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setIsLoading(false)
+      setUser(user)
+    })
+
+    return unsubscribe
+  }, [])
+
+  if (!fontsLoaded | isLoading) return <Loading />
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Welcome" component={Welcome}/>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Register" component={Register}/>
-      </Stack.Navigator>
+      {user ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home}/>
+        </Stack.Navigator> 
+      ) : (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Welcome" component={Welcome}/>
+          <Stack.Screen name="Login" component={Login}/>
+          <Stack.Screen name="Register" component={Register}/>
+        </Stack.Navigator>
+      )}
+      
     </NavigationContainer>
   );
 }
