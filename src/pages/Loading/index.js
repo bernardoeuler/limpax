ecf723ede4affe4a18ae8ed858c945931ebdf24b
Bernardo Euler <rasta.dev07@gmail.com>
2022-07-15 21:52:42 -0300

    Create new screen component Loading

---
+++ src/pages/Loading/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import {
+  Center,
+  NativeBaseProvider,
+  Spinner,
+} from "native-base"
+
+import theme from "../../config/theme"
+
+function Loading() {
+  return (
+    <NativeBaseProvider theme={theme}>
+      <Center flex="1">
+        <Spinner color="neutral.50" size={64}/>
+      </Center>
+    </NativeBaseProvider>
+  )
+}
+
+export default Loading
