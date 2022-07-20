83d4267b2ca686a895dda8e93b8cacdec0d306c8
Bernardo Euler <rasta.dev07@gmail.com>
2022-07-20 08:52:38 -0300

    Remove NBProvider and StatusBar from Register screen

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import {
-  NativeBaseProvider,
   Heading,
   Text,
   Image as NBImage,
@@ -13,16 +12,15 @@ import {
   Divider,
   Icon
 } from "native-base"
-import { SafeAreaView, StatusBar } from "react-native"
-import { MaterialIcons } from '@expo/vector-icons'
-import theme from "../../config/theme"
+import { SafeAreaView } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
 import styles from "../../styles/global"
 import Input from "../../components/Input"
 import FormErrorMessage from "../../components/FormErrorMessage"
 import { auth } from "../../config/firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth"
-import validateEmail from '../../utils/validateEmail'
-import validatePassword from '../../utils/validatePassword'
+import validateEmail from "../../utils/validateEmail"
+import validatePassword from "../../utils/validatePassword"
 import storeData from "../../utils/storeData"
 
 function Register({ navigation }) {
@@ -153,45 +151,41 @@ function Register({ navigation }) {
   }
   
   return (
-    <NativeBaseProvider theme={theme}>
-      <SafeAreaView style={{...styles.SafeAreaView, alignItems: "center", justifyContent: "flex-start", paddingHorizontal: 24}}>
-        <StatusBar barStyle="dark-content" backgroundColor="white"/>
+    <SafeAreaView style={{...styles.SafeAreaView, alignItems: "center", justifyContent: "flex-start"}}>
+      <NBImage mt={10} source={require("../../assets/images/logo.png")} alt="Logo" size={16} />
 
-        <NBImage mt={10} source={require("../../assets/images/logo.png")} alt="Logo" size={16} />
+      <Heading mt={6} size="h3" alignSelf="flex-start">Crie uma conta</Heading>
 
-        <Heading mt={6} size="h3" alignSelf="flex-start">Crie uma conta</Heading>
+      <Box mt={6} w="100%">
+        <VStack space={4}>
+          <Input errorMessage={nameError} onChangeText={(text) => setName(text)} placeholder="Nome completo" InputLeftElement={<Icon as={<MaterialIcons name="person" />} ml={4} size={7} color="neutral.500" />} />
+          <Input errorMessage={emailError} onChangeText={(text) => setEmail(text)} placeholder="Email" InputLeftElement={<Icon as={<MaterialIcons name="email" />} ml={4} size={7} color="neutral.500" />} />
+          <Input errorMessage={passwordError} onChangeText={(text) => setPassword(text)} placeholder="Senha" type={passwordVisible ? "text" : "password"} InputLeftElement={<Icon as={<MaterialIcons name="lock" />} ml={4} size={7} color="neutral.500" />} InputRightElement={<Icon as={<MaterialIcons name={passwordVisible ? "visibility" : "visibility-off"} />} mr={4} size={7} color={passwordVisible ? "primary.500" : "neutral.500"} onPress={() => setPasswordVisible(!passwordVisible)} />} />
+          <Input errorMessage={passwordConfirmationError} onChangeText={(text) => setPasswordConfirmation(text)} placeholder="Confirmar senha" type={passwordConfirmationVisible ? "text" : "password"} InputLeftElement={<Icon as={<MaterialIcons name="lock" />} ml={4} size={7} color="neutral.500" />} InputRightElement={<Icon as={<MaterialIcons name={passwordConfirmationVisible ? "visibility" : "visibility-off"} />} mr={4} size={7} color={passwordConfirmationVisible ? "primary.500" : "neutral.500"} onPress={() => setPasswordConfirmationVisible(!passwordConfirmationVisible)} />} />
+        </VStack>
 
-        <Box mt={6} w="100%">
-          <VStack space={4}>
-            <Input errorMessage={nameError} onChangeText={(text) => setName(text)} placeholder="Nome completo" InputLeftElement={<Icon as={<MaterialIcons name="person" />} ml={4} size={7} color="neutral.500" />} />
-            <Input errorMessage={emailError} onChangeText={(text) => setEmail(text)} placeholder="Email" InputLeftElement={<Icon as={<MaterialIcons name="email" />} ml={4} size={7} color="neutral.500" />} />
-            <Input errorMessage={passwordError} onChangeText={(text) => setPassword(text)} placeholder="Senha" type={passwordVisible ? "text" : "password"} InputLeftElement={<Icon as={<MaterialIcons name="lock" />} ml={4} size={7} color="neutral.500" />} InputRightElement={<Icon as={<MaterialIcons name={passwordVisible ? "visibility" : "visibility-off"} />} mr={4} size={7} color={passwordVisible ? "primary.500" : "neutral.500"} onPress={() => setPasswordVisible(!passwordVisible)} />} />
-            <Input errorMessage={passwordConfirmationError} onChangeText={(text) => setPasswordConfirmation(text)} placeholder="Confirmar senha" type={passwordConfirmationVisible ? "text" : "password"} InputLeftElement={<Icon as={<MaterialIcons name="lock" />} ml={4} size={7} color="neutral.500" />} InputRightElement={<Icon as={<MaterialIcons name={passwordConfirmationVisible ? "visibility" : "visibility-off"} />} mr={4} size={7} color={passwordConfirmationVisible ? "primary.500" : "neutral.500"} onPress={() => setPasswordConfirmationVisible(!passwordConfirmationVisible)} />} />
-          </VStack>
+        <NBButton onPress={() => handleRegister(name, email, password, passwordConfirmation, userTermsAccepted)} mt={4} size="medium">Criar conta</NBButton>
 
-          <NBButton onPress={() => handleRegister(name, email, password, passwordConfirmation, userTermsAccepted)} mt={4} size="medium">Criar conta</NBButton>
+        <HStack mt={4} space={2}>
+          <Checkbox onPress={() => setUserTermsAccepted(!userTermsAccepted)} value="terms" accessibilityLabel="I accept the user terms" />
+          <Text size="small">Eu aceito os termos do usuário</Text>
+        </HStack>
+        <FormErrorMessage errorMessage={userTermsError} />
 
-          <HStack mt={4} space={2}>
-            <Checkbox onPress={() => setUserTermsAccepted(!userTermsAccepted)} value="terms" accessibilityLabel="I accept the user terms" />
-            <Text size="small">Eu aceito os termos do usuário</Text>
-          </HStack>
-          <FormErrorMessage errorMessage={userTermsError} />
+        <HStack my={8} space={1} alignItems="center" justifyContent="center">
+          <Divider w={8} h={0.5} />
+          <Text alignSelf="center" size="small" fontWeight="semibold" color="neutral.300">OU</Text>
+          <Divider w={8} h={0.5} />
+        </HStack>
+        
+        <HStack justifyContent="space-between">
+          <IconButton w="49%" borderWidth="1px" borderColor="neutral.100" _pressed={{bg: "neutral.50"}} icon={<NBImage source={require("../../assets/images/logo-google.png")} size={5} alt="google" />} />
+          <IconButton w="49%" bg="#1976D2" _pressed={{bg: "#1666B6"}} icon={<NBImage source={require("../../assets/images/logo-facebook.png")} size={5} alt="google" />} />
+        </HStack>
+      </Box>
 
-          <HStack my={8} space={1} alignItems="center" justifyContent="center">
-            <Divider w={8} h={0.5} />
-            <Text alignSelf="center" size="small" fontWeight="semibold" color="neutral.300">OU</Text>
-            <Divider w={8} h={0.5} />
-          </HStack>
-          
-          <HStack justifyContent="space-between">
-            <IconButton w="49%" borderWidth="1px" borderColor="neutral.100" _pressed={{bg: "neutral.50"}} icon={<NBImage source={require("../../assets/images/logo-google.png")} size={5} alt="google" />} />
-            <IconButton w="49%" bg="#1976D2" _pressed={{bg: "#1666B6"}} icon={<NBImage source={require("../../assets/images/logo-facebook.png")} size={5} alt="google" />} />
-          </HStack>
-        </Box>
-
-        <Text mt={4} size="small">Já tem uma conta? <Text color="primary.500" fontWeight="bold" onPress={() => {navigation.navigate("Login")}}>Faça login</Text></Text>
-      </SafeAreaView>
-    </NativeBaseProvider>   
+      <Text mt={4} size="small">Já tem uma conta? <Text color="primary.500" fontWeight="bold" onPress={() => {navigation.navigate("Login")}}>Faça login</Text></Text>
+    </SafeAreaView>
   )
 }
 
