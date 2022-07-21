a94ef272c730d87af62ee9244e246632a5adbc75
Bernardo Euler <rasta.dev07@gmail.com>
2022-07-21 11:20:45 -0300

    Add drawer navigation and put Home inside it

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,42 +1,14 @@
 import React from "react"
-import { HamburgerIcon } from "native-base"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import Map from "../screens/Map"
-import NewDenunciation from "../screens/NewDenunciation"
-import theme from "../config/theme"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import HomeStack from "../screens/Home"
 
 function AppRoutes() {
-  const Stack = createNativeStackNavigator()
-  const { colors } = theme
+  const Drawer = createDrawerNavigator()
 
   return (
-    <Stack.Navigator 
-      screenOptions={{
-        headerTitle: "Mapa de denúncias",
-        headerTitleAlign: "left",
-        headerTintColor: colors.neutral[900]
-      }}
-    >
-      <Stack.Screen
-        name="Map"
-        component={Map}
-        options={{
-          headerRight: () => <HamburgerIcon onPress={() => navigation.openDrawer()} color="neutral.900" size={8} />,
-          headerTitle: "Mapa de denúncias",
-        }}
-      />
-      <Stack.Screen
-        name="NewDenunciation"
-        component={NewDenunciation}
-        options={{
-          headerTitle: "Nova denúncia",
-          headerTintColor: colors.white,
-          headerStyle: {
-            backgroundColor: colors.primary[500]
-          }
-        }}
-      />
-    </Stack.Navigator> 
+    <Drawer.Navigator screenOptions={{headerShown: false}}>
+      <Drawer.Screen name="Home" component={HomeStack} />
+    </Drawer.Navigator>
   )
 }
 
