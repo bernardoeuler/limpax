d69139d7dd71c1af067c6ff2f4d39429ee522cd2
Bernardo Euler <rasta.dev07@gmail.com>
2022-07-26 21:56:35 -0300

    update storeData()

--- src/utils/storeData.js
+++ src/utils/storeData.js
@@ -1,10 +1,17 @@
 import { firestore } from "../config/firebase"
 import { addDoc, getDoc, collection } from "firebase/firestore"
 
-export default function storeData(dataObject, collectionPath) {
+export default async function storeData(dataObject, collectionPath) {
   const collectionRef = collection(firestore, collectionPath)
-  addDoc(collectionRef, dataObject)
-    .then(docRef => getDoc(docRef)
-      .then(doc => console.log("Document ID:", doc.id)))
-    .catch(err => console.error("Error: ", err))
+
+  try {
+    const docRef = await addDoc(collectionRef, dataObject)
+    const doc = await getDoc(docRef)
+    return doc.id
+  }
+
+  catch (err) {
+    console.error("Error: ", err)
+    throw Error(err)
+  }
 }
