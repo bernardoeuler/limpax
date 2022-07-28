198b693ca3ee16598456be8007412ddbd556d922
Bernardo Euler <rasta.dev07@gmail.com>
2022-07-27 22:51:26 -0300

    Fetch the images uploaded and render them

--- src/screens/DenunciationDetails/index.js
+++ src/screens/DenunciationDetails/index.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from "react"
-import { StatusBar, ScrollView, VStack, Pressable, Image, Heading, Text } from "native-base"
+import { StatusBar, ScrollView, VStack, Pressable, Image, Heading, Text, FlatList, Center, Box } from "native-base"
 import { Dimensions } from "react-native"
 import styles from "../../styles/global"
 import theme from "../../config/theme"
-import { storage } from "../../config/firebase"
-import { ref, getDownloadURL } from "firebase/storage"
+import getSpecificDoc from "../../utils/getSpecificDoc"
+import { auth, firestore } from "../../config/firebase"
+import { getDocs, collection } from "firebase/firestore"
 
 function DenunciationDetails({ route }) {
   const { colors } = theme
   const { documentId: denunciationId, status, garbageType, quantity, description, date } = route.params
   const imageFullWidth = Dimensions.get("window").width - styles.Container.paddingHorizontal * 2
   const statusText = status === "pending" ? "Em andamento" : "Resolvida"
-  const [fisrtImageUrl, setFirstImageUrl] = useState(null)
+  const [images, setImages] = useState([])
 
   useEffect(() => {
-    const fisrtImageRef = ref(storage, `images/denunciations/${denunciationId}/img1`)
-    getDownloadURL(fisrtImageRef)
-      .then(url => setFirstImageUrl(url))
+    async function getImages() {
+      const authenticatedUserId = auth.currentUser.uid
+      const usersRef = collection(firestore, "users")
+      const userDoc = await getSpecificDoc(usersRef, "userId", authenticatedUserId)
+      const imagesCollectionRef = collection(firestore, `users/${userDoc.documentId}/denunciations/${denunciationId}/images`)
+      const { docs: imagesDocs } = await getDocs(imagesCollectionRef)
+      const imagesArray = imagesDocs.map(doc => {
+        return { ...doc.data(), documentId: doc.id }
+      })
+      setImages(imagesArray)
+    }
+
+    getImages()
   }, [])
 
 
@@ -25,9 +36,12 @@ function DenunciationDetails({ route }) {
       <StatusBar barStyle="light-content" backgroundColor={colors.primary[500]} />
 
       <VStack space={8} my={6}>
-        <Pressable bg="neutral.50" borderRadius={16} overflow="hidden">
-          <Image source={{uri: fisrtImageUrl}} width={imageFullWidth} height={200} resizeMode="cover" alt="uploaded image" />
-        </Pressable>
+        <VStack space={3}>
+          <Heading size="h6">Localização</Heading>
+          <Pressable bg="neutral.50" borderRadius={16} overflow="hidden">
+            <Image source={{uri: null}} width={imageFullWidth} height={200} resizeMode="cover" alt="Mapa" />
+          </Pressable>
+        </VStack>
 
         <VStack space={1}>
           <Heading size="h6">Status da denúncia</Heading>
@@ -52,6 +66,25 @@ function DenunciationDetails({ route }) {
         <VStack space={1}>
           <Heading size="h6">Descrição</Heading>
           <Text color="neutral.600" fontWeight="medium">{description}</Text>
+        </VStack>
+
+        <VStack space={3}>
+          <Heading size="h6">Fotos</Heading>
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={[...images, ...images]}
+            renderItem={({item}) => {
+              console.log(item.url)
+              return (
+                <Center key={item.documentId} position="relative" flex={1} w={24} h={24} bg="neutral.50" borderRadius={8} overflow="hidden">
+                  <Image source={{ uri: item.url }} size={24} resizeMode="cover" alt="Image" />
+                </Center>
+              )
+            }}
+            ItemSeparatorComponent={() => <Box w={4} />}
+          >
+          </FlatList>
         </VStack>
       </VStack>
     </ScrollView>
