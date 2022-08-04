40b2e0e8eea17e6fbe7aecf516d3255b4cb9099a
Bernardo Euler <77172676+bernardoeuler@users.noreply.github.com>
2022-08-03 23:12:22 -0300

    Update parseTimestamp.js

--- src/utils/parseTimestamp.js
+++ src/utils/parseTimestamp.js
@@ -1,6 +1,6 @@
 export default function parseTimestamp(timestamp) {
   const date = new Date(timestamp)
-  const day = date.getDate()
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
   const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
   const year = date.getFullYear()
   const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
