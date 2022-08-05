a507c3f2c65bbedb0926eedab6aa265b15a5583f
Bernardo Euler <77172676+bernardoeuler@users.noreply.github.com>
2022-08-05 10:05:50 -0300

    Revert "fix: MapView static map (#36)" (#37)
    This reverts commit 48393b1147ff15ee7674831479b548f3930fdc1a.

--- src/utils/validateEmail.js
+++
@@ -1,6 +0,0 @@
-export default function validateEmail(email) {
-  const validEmailExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  const isValid = validEmailExpression.test(email.toLowerCase())
-  
-  return !isValid
-}
